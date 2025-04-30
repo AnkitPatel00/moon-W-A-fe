cafcea5b057cc0bf258c9ac91df355add08f72b1
AnkitPatel00 <ankitpatel.stockimage@gmail.com>
2025-04-30 11:06:31 +0530

    strictMode

--- src/main.jsx
+++ src/main.jsx
@@ -82,7 +82,9 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <RouterProvider router={router} />
-  </Provider>
+  <StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
 );
